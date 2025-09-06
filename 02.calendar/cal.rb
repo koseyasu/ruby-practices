3fb89dfac3f5278a8b76ff5245caf9702c605814
kose yasunori <yasunori04235@gmail.com>
2025-09-06 11:00:59 +0900

    Fix calendar

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -5,32 +5,25 @@ require 'date'
 
 params = ARGV.getopts("m:y:")
 
-if params["y"] == nil
-  cal_year = Date.today.year
-else
-  cal_year = params["y"].to_i
-end
-
-if params["m"] == nil
-  cal_month = Date.today.month
-else
-  cal_month = params["m"].to_i
-end
+cal_today = Date.today
+cal_year = params["y"] == nil ? cal_today.year : params["y"].to_i
+cal_month = params["m"] == nil ? cal_today.month : params["m"].to_i
 
 firstday = Date.new(cal_year,cal_month,1)
-lastday = (firstday.next_month(1)-1).day
+lastday = Date.new(cal_year,cal_month,-1).day
 
 day_of_week = ["日","月","火","水","木","金","土"]
 firstday_of_week = firstday.wday
 display_area = firstday_of_week + lastday
 
-if display_area > 35
-  cal_row = 7
-elsif display_area > 28
-  cal_row = 6
-else
-  cal_row = 5
-end
+cal_row =
+  if display_area > 35
+    7
+  elsif display_area > 28
+    6
+  else
+    5
+  end
 
 cal_blocks = Array.new(cal_row){Array.new(7)}
 day_counter = 1
@@ -39,13 +32,8 @@ cal_blocks.each_with_index do |row_item,i|
   row_item.each_with_index do |col_item,j|
     if i == 0
       cal_blocks[i][j] = day_of_week[j] 
-    elsif i == 1
-      if j < firstday_of_week
-        cal_blocks[i][j] = "  "
-      else
-        cal_blocks[i][j] = format("%2d",day_counter)
-        day_counter += 1
-      end
+    elsif i == 1 && j < firstday_of_week
+      cal_blocks[i][j] = "  "
     else
       cal_blocks[i][j] = format("%2d",day_counter) if day_counter <= lastday
       day_counter += 1
